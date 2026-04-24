d6d9a5d446ed6b3b6759b261a0600174bcdaf8a8
Abi-Sibi <abidashnia@gmail.com>
2026-04-24 20:18:01 +0530

    Add evaluation function for model performance metrics

---
+++ data/src/src/src/evaluate.py
@@ -0,0 +1,7 @@
+from sklearn.metrics import mean_absolute_error, r2_score
+
+def evaluate(model, X_test, y_test):
+    predictions = model.predict(X_test)
+    mae = mean_absolute_error(y_test, predictions)
+    r2 = r2_score(y_test, predictions)
+    return mae, r2
